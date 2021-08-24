0ac1fd1594d3683d302f574eced00b3e08db792f
lightdaysofback <1607762493@qq.com>
2021-08-24 11:02:27 +0800

    springcloud

---
+++ cloudclient/src/test/java/com/nsk/cloudclient/CloudclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsk.cloudclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ cloudclient1/src/test/java/com/nsk/cloudclient1/Cloudclient1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsk.cloudclient1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cloudclient1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
